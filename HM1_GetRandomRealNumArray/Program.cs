dffc06fde776fc83f564c89ea57627b302554629
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-31 08:57:13 +0300

    HM1_GetRandomRealNumArray

---
+++ HM1_GetRandomRealNumArray/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив m, n с рандомными
+// вещественными элементами в диапазоне 
+// между -9,9 и 9,9 с 1 знаком после запятой
+
+Console.WriteLine("Задайте число строк массива m");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте число строк массива n");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+double [,] matrix = new double[rows, columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+  for (int j = 0; j < matrix.GetLength(1); j++)
+  {
+    matrix[i, j] = new Random().Next(-9, 10) + new Random().Next(-9, 10) * 0.1;
+    Console.Write(matrix[i, j] + " ");
+  }
+  Console.WriteLine();
+}
